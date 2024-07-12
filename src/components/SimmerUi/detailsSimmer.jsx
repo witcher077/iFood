fde89a9dc0e8097183d9936fb50af146eddf1046
witcher077 <ashokkumarmahto2000@gmail.com>
2024-07-13 00:05:09 +0530

    bug fix

--- src/components/SimmerUi/detailsSimmer.jsx
+++ src/components/SimmerUi/detailsSimmer.jsx
@@ -9,52 +9,49 @@ const Detailsimmer = () => {
   theme==="moon"?element.classList.add("dark"):element.classList.remove("dark");
     }, [theme])
   return (
-    <div className='dark:bg-gray-800' >
-        <div className="m-4 rounded-lg py-4  w-52  h-9  bg-slate-100 dark:bg-gray-600 shadow-sm ">
+    <div className='dark:bg-gray-800 w-6/12 m-auto' >
+        <div className="m-4 rounded-lg py-4  w-6/12  h-9  bg-slate-100 dark:bg-gray-600 shadow-sm ">
 
         </div>
-         <div className="flex justify-between flex-wrap">
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
-        <div className=' w-44 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-600 shadow-sm rounded-lg' ></div>
+         <div className="flex  flex-col">
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
+        <div className=' w-100 h-52 m-4 p-4  bg-slate-100 dark:bg-gray-400 shadow-sm rounded-lg' ></div>
         </div>
        
     </div>
